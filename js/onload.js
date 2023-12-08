7b5ef95ca8a69a892430d9f4569ec1060b2605ea
jin.seungwan <soma0sd@gmail.com>
2023-12-08 18:32:28 +0900

    컨텐츠 업데이트

--- js/onload.js
+++ js/onload.js
@@ -1,4 +1,19 @@
 function toggleNav() {
-    document.getElementById("toc-sidebar").classList.toggle("active");
-    document.getElementById("main-contents").classList.toggle("open-sidebar");
+  document.getElementById("toc-sidebar").classList.toggle("active");
+  document.getElementById("main-contents").classList.toggle("open-sidebar");
 }
+
+function showImageAlt() {
+  document.querySelectorAll("#article img").forEach(function (img) {
+    if (!img.alt) return;
+    let altText = img.alt;
+    let altTextElement = document.createElement("div");
+    altTextElement.classList.add("alt-text");
+    altTextElement.innerHTML = altText;
+    img.parentNode.insertBefore(altTextElement, img.nextSibling);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  showImageAlt();
+});
